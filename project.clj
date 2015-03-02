0577fc61b954f1e9f3f6c7868cfe3d8c0b02a472
conan <conan.cook@gmail.com>
2015-03-02 23:01:17 +0000

    Getting sprints

--- project.clj
+++ project.clj
@@ -1,6 +1,13 @@
 (defproject jira-burndown "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+
+  :description "Tool to extract JIRA Agile's burndown chart"
+
+  :url "https://github.com/conan/jira-burndown"
+
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[cheshire "5.4.0"]
+                 [clj-http "1.0.1"]
+                 [environ "1.0.0"]
+                 [org.clojure/clojure "1.6.0"]])
--- src/jira_burndown/core.clj
+++ src/jira_burndown/core.clj
@@ -1,6 +1,29 @@
-(ns jira-burndown.core)
+(ns jira-burndown.core
+  (:require [clj-http.client :as client]
+            [environ.core :refer [env]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(def host (env :jira-host))
+(def base-url (str "https://" host))
+(def creds [(env :jira-username) (env :jira-password)])
+
+
+(def cfg {:throw-exceptions false
+          :as               :json
+          :basic-auth       creds})
+
+
+(defn get-active-sprints
+  [rapid-view-id]
+  (let [response (client/get (str base-url "/rest/greenhopper/1.0/sprintquery/" rapid-view-id) cfg)
+        sprints  (get-in response [:body :sprints])]
+    (filter #(= (:state %) "ACTIVE") sprints)))
+
+
+(defn get-sprint-ids
+  [sprints]
+  (map :id sprints))
+
+
+(defn get-burndown
+  (client/get (str base-url "/rest/greenhopper/1.0/rapid/charts/releaseburndownchart?rapidViewId=3&versionId=10100") cfg))
